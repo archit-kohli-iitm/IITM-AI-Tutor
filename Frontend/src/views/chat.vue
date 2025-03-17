0b605d186b5cac29f9ebf1ee1b9eda041b24de05
harshpal1441 <harshpalaps1@gmail.com>
2025-03-17 21:42:28 +0530

    added new pages and routes

--- Frontend/src/views/chat.vue
+++ Frontend/src/views/chat.vue
@@ -8,7 +8,7 @@
           <img src="../assets/image.png" alt="IITM Logo" class="logo me-2" width="50" height="50"/>
           <button class="btn btn-light border-dark ms-2" @click="reloadPage">Refresh</button>
         </div>
-        
+  
         <span class="navbar-brand mb-0 h1 text-dark">Welcome to the AI Tutor, Sandeep</span>
         
         <router-link to="/" class="btn btn-light border-dark">Log out</router-link>
@@ -17,7 +17,7 @@
       
       <div class="row flex-grow-1">
         <!-- Sidebar -->
-        <div class="col-3 bg-white  d-flex flex-column p-3">
+        <div class="col-3 bg-white d-flex flex-column p-3">
           <h5 class="text-dark mb-4 text-center" style="margin-top: 20px;">Subjects</h5>
           <div v-for="(chats, subject) in chats" :key="subject" class="mb-4">
             <div class="d-flex justify-content-center mb-1">
@@ -28,24 +28,37 @@
                 style="background-color: #ffffff; color: black;">
                 {{ subject }}
               </button>
-
             </div>
           </div>
         </div>
         
         <!-- Chat Area -->
         <div class="col-9 d-flex flex-column bg-light p-3 border">
+          
+          <!-- New Button Above Chat Box -->
+          <div class="d-flex justify-content-center mb-3">
+            <button 
+              class="btn btn-warning w-50 text-dark" 
+              @click="goToWeekLecture">
+              📚 Week Lectures
+            </button>
+          </div>
+  
           <div class="flex-grow-1 border rounded p-3 bg-white">
             <p v-if="selectedChat">Chatting in: {{ selectedChat }}</p>
             <p v-else>Select a subject to start conversation ...</p>
+
             <div v-for="(message, index) in messages" :key="index" class="mt-2">
+                
               <p class="mb-1">{{ message }}</p>
             </div>
           </div>
+
           <div class="d-flex mt-3">
             <input v-model="query" type="text" class="form-control border border-dark" placeholder="Enter your query here ........" @keyup.enter="askQuestion"/>
             <button class="btn btn-warning ms-2" @click="askQuestion">Ask</button>
           </div>
+  
         </div>
         
       </div>
@@ -75,12 +88,15 @@
       askQuestion() {
         if (this.query.trim()) {
           this.messages.push(`You: ${this.query}`);
-          this.messages.push(`AI Tutor: You posted the following query -- ${this.query} --. This is a dummy reponse to your query.`);
+          this.messages.push(`AI Tutor: You posted the following query -- ${this.query} --. This is a dummy response to your query.`);
           this.query = '';
         }
       },
       reloadPage() {
-        window.location.reload();  // This reloads the current page
+        window.location.reload();
+      },
+      goToWeekLecture() {
+        this.$router.push('/week-lectures'); // Make sure this route is defined in your router
       }
     }
   };
